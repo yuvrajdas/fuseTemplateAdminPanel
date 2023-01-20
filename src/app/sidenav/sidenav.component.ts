6185cef530d8e7f23ffe49f4db108070bd903774
yuvraj <yuvrajdas1531@gmail.com>
2023-01-20 19:23:03 +0530

    sidebar development

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -10,6 +10,7 @@ export class SidenavComponent implements OnInit {
   isAuthList = false;
   isInvoiceList = false;
   isshowPageLayoutLists = false;
+  isShowLeve1 = false;
   ngOnInit(): void {}
   showAuthenticationLists() {
     this.isAuthList = !this.isAuthList;
@@ -20,4 +21,8 @@ export class SidenavComponent implements OnInit {
   showPageLayoutLists() {
     this.isshowPageLayoutLists = !this.isshowPageLayoutLists;
   }
+  showLevel1() {
+    this.isShowLeve1 = !this.isShowLeve1;
+  }
+  showLevel2() {}
 }
